2e2bc70c3e526384d828222b0599818f2424cda8
binay <binay885b@gmail.com>
2026-04-17 18:31:40 +0530

    api changes for production

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Change this baseURL to your backend URL
 const api = axios.create({
-  baseURL: "http://localhost:5000/api", // your backend URL
+  baseURL: "https://dogsafety-z6t2.onrender.com/api", // your backend URL
   headers: {
     "Content-Type": "application/json",
   },
